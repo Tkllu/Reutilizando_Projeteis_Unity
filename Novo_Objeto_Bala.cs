aa224dbe5fc105a18eed1dbe3c5c988f371455cb
Tkllu <hozana157binladen@gmail.com>
2017-03-19 14:35:16 -0300

    Add files via upload

---
+++ Novo_Objeto_Bala.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Novo_Objeto_Bala : MonoBehaviour {
+
+	public static Novo_Objeto_Bala acessar_Bala;
+	public GameObject bala;
+	public int quant_max_bala;
+	public List<GameObject> list_Bala;
+	public int i = 0;
+	void Awake(){
+		acessar_Bala = this;
+	}
+
+	void Start () {
+		
+		list_Bala = new List<GameObject> ();
+		for(int i = 0; i < quant_max_bala; i++){
+
+			GameObject obj = (GameObject)Instantiate (bala);
+			obj.SetActive (false);
+			list_Bala.Add (obj);
+		}
+
+	}
+
+	public GameObject RetornaObj(){
+
+		if (i < quant_max_bala) {
+
+			if (!list_Bala [i].activeInHierarchy) {
+				return list_Bala [i];
+			}
+			i = i + 1;
+		} else {
+			i = 0;
+		}
+
+		return null;
+	}
+
+}
---
+++ Vel_Bala.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vel_Bala : MonoBehaviour {
+
+	public int vel_tiro;
+
+	void Start (){
+
+	}
+
+	void Update () {
+		
+		transform.Translate (0,0, vel_tiro * Time.deltaTime);
+
+		if (transform.position.x > 40) {
+			Destruir ();
+		}
+	}
+
+	void OnTriggerEnter(Collider outro){
+
+		if(outro.CompareTag("Meteoro")){
+			Destruir ();
+		}
+		if(outro.CompareTag("Inimigo")){
+			Destruir ();
+		}
+	}
+
+	void Destruir(){
+
+		gameObject.SetActive (false);
+
+	}
+
+	}
